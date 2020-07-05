495ab0384c8a2479463a22e9f781d23ce5bab45e
ViataF <viataruth@gmail.com>
2020-07-05 23:14:30 +0200

    Navbar

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,37 +1,81 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="nav-wrapper sticky">
-        <a href="#!" className="brand-logo">
-          {" "}
-          <i className="material-icons">colorize</i>
-          ViataF
-        </a>
-        <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-          <i className="material-icons">menu</i>
-        </a>
-        <ul className="right hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Intro</a>
-          </li>
-          <li>
-            <a href="badges.html">Portfolio</a>
-          </li>
-          <li>
-            <a href="collapsible.html">About</a>
-          </li>
-          <li>
-            <a href="mobile.html">Testimonials</a>
-          </li>
-          <li>
-            <a href="mobile.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <header>
+      <nav>
+        <div className="nav-wrapper ">
+          <a
+            href="/"
+            className="brand-logo"
+            style={{ fontSize: "100%", color: "black" }}
+          >
+            <i className="medium material-icons">developer_mode</i>
+            Viata Fredericks
+          </a>
+          <a href="/" data-target="mobile-demo" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <Link to="/">Intro</Link>
+            </li>
+            <li>
+              <Link to="/about">About Me</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/testimonial">Testimonial</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile-demo">
+        <li>
+          <Link to="/">Intro</Link>
+        </li>
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/portfolio/">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/testimonial">Testimonial</Link>
+        </li>
+        <footer className="navbar-footer">
+          <a
+            href="https://github.com/ViataF"
+            className="waves-effect waves-light btn-large  grey darken-3"
+          >
+            <i className="fa fa-github" />
+          </a>
+          <a
+            href="https://za.linkedin.com/in/viata-fredericks-2580151ab"
+            className="waves-effect waves-light btn-large  light-blue darken-4 "
+          >
+            <i className="fa fa-linkedin" />
+          </a>
+          <a
+            href="https://gmail.com"
+            className="waves-effect waves-light btn-large  deep-orange darken-3"
+          >
+            <i className="fa fa-google" />
+          </a>
+        </footer>
+      </ul>
+    </header>
   );
 };
+
 export default Navbar;
