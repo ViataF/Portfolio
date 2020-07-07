bbf5f9195ccbb952db2fcba7be6dd2185017ceee
ViataF <viataruth@gmail.com>
2020-07-07 16:44:04 +0200

    testimonial added

--- src/components/layout/pages/Testimonials.js
+++ src/components/layout/pages/Testimonials.js
@@ -68,6 +68,21 @@ const Testimonials = () => {
           </div>
         </div>
       </div>
+      <div className="col s12 m6">
+        <div className="card red">
+          <div className="card-content black-text">
+            <span className="card-title" style={Style}>
+              Godwin Dzvapatsva (Head of Curriculum and Learning)
+            </span>
+            <p className="white-text">
+              Viata has proven to be an all-rounder in software development. She
+              was very professional and easy to work with throughout her stay at
+              Lifechoices including being responsive. I recommend her without
+              doubt.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
