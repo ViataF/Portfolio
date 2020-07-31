c84f78e96b91c5f65b5d67c9b99c31cbdf34f987
ViataF <viataruth@gmail.com>
2020-07-31 11:31:14 +0200

    Updates

---
+++ precache-manifest.144174effd64b1020cdbac780a2c7584.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d99ee9a6d2eac39b002065ff137ef8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d7671a6ffa8ca95a02b0",
+    "url": "/Portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "0e12df647b6bc85dd4a0",
+    "url": "/Portfolio/static/css/main.5d84a4c1.chunk.css"
+  },
+  {
+    "revision": "d7671a6ffa8ca95a02b0",
+    "url": "/Portfolio/static/js/2.eea69fd3.chunk.js"
+  },
+  {
+    "revision": "4fef53b365fb409a8d4059530325da23",
+    "url": "/Portfolio/static/js/2.eea69fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e12df647b6bc85dd4a0",
+    "url": "/Portfolio/static/js/main.74a6c15e.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "0547178cdfb4bdca7671a354aab76b40",
+    "url": "/Portfolio/static/media/Login-system.0547178c.PNG"
+  },
+  {
+    "revision": "e3dceca503391918586c6e5257bfc94a",
+    "url": "/Portfolio/static/media/Malvie.e3dceca5.otf"
+  },
+  {
+    "revision": "e9060109c3a1b4272a79c25016bfaf95",
+    "url": "/Portfolio/static/media/github_finder.e9060109.jpg"
+  },
+  {
+    "revision": "0c4baeeb40380430589885b69a6e8cb9",
+    "url": "/Portfolio/static/media/it-logger.0c4baeeb.PNG"
+  },
+  {
+    "revision": "cce248863379ffd313ad42ea234a358e",
+    "url": "/Portfolio/static/media/me1.cce24886.jpg"
+  }
+]);
